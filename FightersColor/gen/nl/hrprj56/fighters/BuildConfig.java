48ff92365d9c7a18a5037ab5eb7c95925b2cc76e
Paulo <paulo.deen@gmail.com>
2012-04-14 21:10:16 +0200

    New Android project

---
+++ FightersColor/gen/nl/hrprj56/fighters/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package nl.hrprj56.fighters;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ FightersColor/src/nl/hrprj56/fighters/FightersActivity.java
@@ -0,0 +1,13 @@
+package nl.hrprj56.fighters;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FightersActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
