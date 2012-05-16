db565000f5e271ec308182d51faa4a595c89e62c
Fatih Aytar <aytar.fatih@gmail.com>
2012-05-16 16:52:44 +0200

    menu werkend

--- FightersColor/gen/nl/hrprj56/fighters/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package nl.hrprj56.fighters;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
---
+++ Game/src/com/hr/fighter/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hr.fighter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main); 
+	}
+		
+}
