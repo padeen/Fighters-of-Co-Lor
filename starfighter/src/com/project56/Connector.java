ed8eb3ac58765fa318f1f0de1def3ea9680f422f
jordyhert <jordyhert@gmail.com>
2012-06-16 12:38:29 +0200

    Connector klasse

---
+++ starfighter/src/com/project56/Connector.java
@@ -0,0 +1,17 @@
+package com.project56;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Connector {
+	
+	
+	public static final int SERVER = 1;
+	public static final int CLIENT = 2;
+	
+	
+	public static int playerSide;
+	public static ObjectOutputStream OOS;
+	public static ObjectInputStream OIS;
+	
+}
